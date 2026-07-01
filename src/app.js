aabd67f418b328fd0de2713ccd109b060b032d35
Prabhat Verma <prabhatsoni0109@gmail.com>
2026-07-01 20:16:38 +0530

    feat: initialize app structure with logger, security middleware, and module path aliases

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
 import express from 'express';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+
+
+app.use(morgan('combined', {
+    stream: {
+        write: (message) =>
+            logger.info(message.trim())
+    }
+}));
+
+
 
 app.get('/', (req, res) => {
-  res.status(200).send('hello from Acquisitions');
+    logger.info("hello from acquisitions!!");
+    res.status(200).send('hello from Acquisitions');
 });
 
 export default app;
